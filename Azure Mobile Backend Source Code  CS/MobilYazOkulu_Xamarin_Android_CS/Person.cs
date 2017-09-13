2e7be2d139fe037a592f1cc84612a5771ba6acb9
KursatCAKAL <kursat.cakal@hotmail.com>
2017-09-13 06:51:34 +0300

    Add files via upload

---
+++ Azure Mobile Backend Source Code  CS/MobilYazOkulu_Xamarin_Android_CS/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Newtonsoft.Json;
+
+namespace MobilYazOkulu
+{
+    class Person
+    {
+        [JsonProperty(PropertyName ="Name")]
+        public string Name { get; set; }
+        [JsonProperty(PropertyName = "Text")]
+        public string Text { get; set; }
+        [JsonProperty(PropertyName = "Age")]
+        public int Age { get; set; }
+    }
+}
